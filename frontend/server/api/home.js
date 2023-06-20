129b449d2ee55b114328a618d1e63944cd543e0a
Kiefer Slaton <kslaton@madg.com>
2023-06-20 09:27:25 -0500

    big push using nuxt build and adding home single

--- frontend/server/api/home.js
+++ frontend/server/api/home.js
@@ -1,18 +1,20 @@
 export default defineEventHandler(async (event) => {
-    const config = useRuntimeConfig();
-    const query = getQuery(event);
+  const config = useRuntimeConfig();
+  const query = getQuery(event);
 
-    let queryString = `&page=${query.page}&perPage=1&searchTerm=${query.name}`
-    let url = `${config.public.regionalApiUrl}/search?storeId=1${queryString}`
+  let queryString = `&page=${query.page}&perPage=1&searchTerm=${
+    query.name.split("-")[0]
+  }`;
+  let url = `${config.public.regionalApiUrl}/search?storeId=1${queryString}`;
 
-    try { 
-        const response = await fetch(url)
-        const data = await response.json();
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
 
-        return data.inventoryItems[0]
-    } catch(e) {
-        return {
-            error: e
-        }
-    }
-})
+    return data.inventoryItems[0];
+  } catch (e) {
+    return {
+      error: e,
+    };
+  }
+});
