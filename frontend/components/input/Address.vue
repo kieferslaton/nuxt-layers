1eb3967e985dceafb5a63d9af23cf7deadf8b86d
Kiefer Slaton <kslaton@madg.com>
2023-07-14 15:12:43 -0500

    incorporating v-select for state dropdown

--- frontend/components/input/Address.vue
+++ frontend/components/input/Address.vue
@@ -9,7 +9,7 @@
       name="address"
       required
       @blur="onBlur('street')"
-      :value="modelValue.street"
+      v-model="modelValue.street"
     />
     <label for="address">Address</label>
     <small v-if="errors?.street?.length"
@@ -26,31 +26,28 @@
       name="city"
       required
       @blur="onBlur('city')"
-      :value="modelValue.city"
+      v-model="modelValue.city"
     />
     <label for="city">City</label>
     <small v-if="errors?.city?.length">{{ errors.city[0]?.$message }}.</small>
   </div>
-  <div class="flex justify-between gap-6">
+  <div class="flex w-full justify-between gap-6">
     <div
-      class="input-container basis_[70%]"
+      class="input-container flex flex-grow flex-col"
       :class="[classes, { error: errors?.state?.length }]"
     >
-      <input
-        type="text"
-        id="state"
-        name="state"
-        required
-        @blur="onBlur('state')"
-        :value="modelValue.state"
-      />
+      <v-select
+        class="w-full"
+        :options="states"
+        v-model="modelValue.state"
+      ></v-select>
       <label for="state">State</label>
-      <small v-if="errors?.state?.length"
-        >{{ errors.state[0]?.$message }}.</small
-      >
+      <small v-if="errors?.state?.length">{{
+        errors.state[0]?.$message
+      }}</small>
     </div>
     <div
-      class="input-container"
+      class="input-container flex flex-shrink basis-[70%] flex-col"
       :class="[classes, { error: errors?.zip?.length }]"
     >
       <input
@@ -59,14 +56,16 @@
         name="zip"
         required
         @blur="onBlur('zip')"
-        :value="modelValue.zip"
+        v-model="modelValue.zip"
       />
       <label for="zip">Zip</label>
-      <small v-if="errors?.zip?.length">{{ errors.zip[0]?.$message }}.</small>
+      <small v-if="errors?.zip?.length">{{ errors.zip[0]?.$message }}</small>
     </div>
   </div>
 </template>
 <script setup>
+import vSelect from "vue-select";
+import "vue-select/dist/vue-select.css";
 const props = defineProps({
   modelValue: Object,
   classes: String,
@@ -106,7 +105,7 @@ onMounted(() => {
       if (addressType === "locality") {
         cityValue = place.address_components[i]["long_name"];
       } else if (addressType === "administrative_area_level_1") {
-        stateValue = place.address_components[i]["short_name"];
+        stateValue = place.address_components[i]["long_name"];
       } else if (addressType === "postal_code") {
         zipValue = place.address_components[i]["short_name"];
       } else if (addressType === "street_number") {
@@ -130,6 +129,60 @@ onMounted(() => {
     document.getElementById("autocomplete").value = streetAddress.trim();
   });
 });
+
+const states = [
+  { label: "Alaska", value: "Alaska" },
+  { label: "Alabama", value: "Alabama" },
+  { label: "Arkansas", value: "Arkansas" },
+  { label: "Arizona", value: "Arizona" },
+  { label: "California", value: "California" },
+  { label: "Colorado", value: "Colorado" },
+  { label: "Connecticut", value: "Connecticut" },
+  { label: "District of Columbia", value: "District of Columbia" },
+  { label: "Delaware", value: "Delaware" },
+  { label: "Florida", value: "Florida" },
+  { label: "Georgia", value: "Georgia" },
+  { label: "Hawaii", value: "Hawaii" },
+  { label: "Iowa", value: "Iowa" },
+  { label: "Idaho", value: "Idaho" },
+  { label: "IL", value: "Illinois" },
+  { label: "Illinois", value: "Indiana" },
+  { label: "Kansas", value: "Kansas" },
+  { label: "Kentucky", value: "Kentucky" },
+  { label: "Louisiana", value: "Louisiana" },
+  { label: "Massachusetts", value: "Massachusetts" },
+  { label: "Maryland", value: "Maryland" },
+  { label: "Maine", value: "Maine" },
+  { label: "Michigan", value: "Michigan" },
+  { label: "Minnesota", value: "Minnesota" },
+  { label: "Missouri", value: "Missouri" },
+  { label: "Mississippi", value: "Mississippi" },
+  { label: "Montana", value: "Montana" },
+  { label: "North Carolina", value: "North Carolina" },
+  { label: "North Dakota", value: "North Dakota" },
+  { label: "Nebraska", value: "Nebraska" },
+  { label: "New Hampshire", value: "New Hampshire" },
+  { label: "New Jersey", value: "New Jersey" },
+  { label: "New Mexico", value: "New Mexico" },
+  { label: "Nevada", value: "Nevada" },
+  { label: "New York", value: "NewYork" },
+  { label: "Ohio", value: "Ohio" },
+  { label: "Oklahoma", value: "Oklahoma" },
+  { label: "Oregon", value: "Oregon" },
+  { label: "Pennsylvania", value: "Pennsylvania" },
+  { label: "Rhode Island", value: "Rhode Island" },
+  { label: "South Carolina", value: "South Carolina" },
+  { label: "South Dakota", value: "South Dakota" },
+  { label: "Tennessee", value: "Tennessee" },
+  { label: "Texas", value: "Texas" },
+  { label: "Utah", value: "Utah" },
+  { label: "Virginia", value: "Virginia" },
+  { label: "Vermont", value: "Vermont" },
+  { label: "Washington", value: "Washington" },
+  { label: "Wisconsin", value: "Wisconsin" },
+  { label: "West Virginia", value: "West Virginia" },
+  { label: "Wyoming", value: "Wyoming" },
+];
 </script>
 <style scoped lang="scss">
 .input-container {
