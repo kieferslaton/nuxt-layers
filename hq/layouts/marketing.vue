5132848465a897d565f888ab777d62d034fb1778
Kiefer Slaton <kslaton@madg.com>
2023-11-10 16:26:36 -0600

    creating new branch to bust WPE's cache

--- hq/layouts/marketing.vue
+++ hq/layouts/marketing.vue
@@ -1,8 +1,6 @@
 <template>
-  <header
-    class="p-row lg:px-row2x absolute z-20 flex w-full items-center justify-center lg:h-[150px] lg:py-0"
-  >
-    <img src="/logos/regional-logo-white.svg" alt="Logo" class="lg:w-[200px]" />
+  <header class="p-row lg:px-row2x absolute z-20 flex w-full items-center justify-center lg:h-[150px] lg:py-0">
+    <img :src="currentLogo" alt="Logo" class="lg:w-[200px]" />
   </header>
   <main>
     <slot />
@@ -14,6 +12,30 @@
 const route = useRoute();
 const isMobile = useIsMobile();
 const isDark = useIsDark(route, isMobile.value);
+const theme = config.public.theme;
+
+const currentLogo = computed(() => {
+  const config = useRuntimeConfig();
+  const theme = config.public.theme;
+  let logoPath;
+
+  switch (theme) {
+    case "Regional":
+      logoPath = "/logos/regional-logo-white.svg"
+      break;
+    case "Town and Country":
+      logoPath = "/logos/tc-logo-white.svg"
+      break;
+    case "brandC":
+      logoPath = "/logos/logo-brandC-white.svg"
+      break;
+    default:
+      logoPath = "/logos/default-logo.svg"; // You can set a default logo if needed
+      break;
+  }
+
+  return logoPath;
+});
 
 useHead({
   bodyAttrs: {
